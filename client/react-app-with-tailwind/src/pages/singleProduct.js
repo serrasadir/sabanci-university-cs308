a5e37d7c43dfd0b035932fceca8b9cf6174fbd48
LAPTOP-NC8B5L1O\sevva <sevvalbulut@sabanciuniv.edu>
2023-06-10 22:14:21 +0300

    single product page

--- client/react-app-with-tailwind/src/pages/singleProduct.js
+++ client/react-app-with-tailwind/src/pages/singleProduct.js
@@ -24,110 +24,105 @@ export const SingleProduct = ({prod}) => {
   
 
   const { state: { cart }, dispatch, } = useContext(Cart);
-
-
   return (
     <div>
-    <div class="w-full bg-gray-200 rounded-md overflow-hidden aspect-w-1 aspect-h-1 min-h-80 lg:aspect-none group-hover:opacity-75 lg:h-80">
-  <Link to={`/products/${prod.product_id}`}>
-  {prod.discount == true ? 
-    (
-      <span class="flex bg-red-500 uppercase px-2 py-1 text-xs font-bold mr-3">%{prod.discount_rate} Discount, SHOP NOW!</span>
-    )
-    :
-    (
-      <div></div>
-    )
-    }
-    <img src={prod.imageUrl} alt={prod.imageAlt} class="w-full h-full object-center object-cover lg:h-full lg:w-full" /> 
-  </Link>
-  </div>
-  
-
-<div class="mt-4 flex justify-between">
-  <div>
-    <h3 class="text-left font-medium text-gray-700">
-      <Link to={`/products/${prod.product_id}`}>{prod.product_name}</Link>
-    </h3>
-    <p class="mt-1 text-sm text-left text-gray-500">{prod.category}</p>
-  </div>
-  {prod.discount == true ? 
-                (
-                <div>
-                    
-                    <p 
-                      style={{textDecoration: 'line-through'}}
-                      cclass="text-right font-medium text-gray-900"
-                      >
-                      {prod.old_price} TL
-                    </p>
-                  <div className="flex-justify-between">
-                    
-                    <p class="text-right font-medium text-gray-900">{prod.price} TL</p>
-                    
-                  </div>
-                </div>
-                )
-                :
-                (
-                    
-                    <div className="space-y-6">
-                      <p class="text-right font-medium text-gray-900">{prod.price} TL</p>
-                    </div>
-                
-                )
-                }
-  
-</div>
-
-<div>
-  {cart.some((p) => p.product_id === prod.product_id) ? (        
-    <button onClick={() => {
-      dispatch({
-        type:"REMOVE_FROM_CART",
-        payload:prod,
-      });
-    }} class="px-4 py-2 font-bold text-white bg-red-500 rounded hover:bg-red-700">
-      Remove From Cart 
-    </button>
-  ) : (
-    prod.stock <= 0 ? (
-      <button class="px-4 py-2 font-semibold text-gray-800 border border-gray-400 rounded cursor-not-allowed opacity-50 bg-white hover:bg-gray-100">
-        No Stock
-      </button>
-    ) : prod.stock == 1 ? (
-      <div class="flex items-center">
-        <p class="text-sm">Only 1 left!</p>
-        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6 ml-2">
-          <path stroke-linecap="round" stroke-linejoin="round" d="M12 9v3.75m9-.75a9 9 0 11-18 0 9 9 0 0118 0zm-9 3.75h.008v.008H12v-.008z" />
-        </svg>
-        <button onClick={() => {
-          dispatch({
-            type:"ADD_TO_CART",
-            payload:prod,
-          });
-        }} class="px-4 py-2 font-bold text-white bg-blue-500 rounded hover:bg-blue-700 ml-auto">
-          Add to Cart 
-        </button>
+      <p className="text-left italic text-sm text-gray-500">{prod.stock} left!</p>
+      <div className="w-full bg-gray-200 rounded-md overflow-hidden aspect-w-1 aspect-h-1 min-h-80 lg:aspect-none group-hover:opacity-75 lg:h-85">
+        
+        <Link to={`/products/${prod.product_id}`}>
+          {prod.discount ? (
+            <span className="flex px-2 py-1 text-xs text-red-600 font-bold mr-3">
+              %{prod.discount_rate} Discount, Shop now!
+            </span>
+          ) : null}
+          
+          <img src={prod.imageUrl} alt={prod.imageAlt} class="w-full h-full object-center object-cover lg:h-full lg:w-full" />
+        </Link>
       </div>
-    ) : (
-      <div class="flex items-center">
-      <p class="text-sm"> {prod.stock} left </p>
-     
-      <button onClick={() => {
-        dispatch({
-          type:"ADD_TO_CART",
-          payload:prod,
-        });
-      }} class="px-4 py-2 font-bold text-white bg-blue-500 rounded hover:bg-blue-700">
-        Add to Cart 
-      </button>
-      </div>
-    )
-  )}
-</div>
-</div>
   
-    
-  )
-}
+      <div className="mt-4 flex justify-between">
+        <div>
+          <h3 className="text-left font-medium text-gray-700">
+            <Link to={`/products/${prod.product_id}`}>{prod.product_name}</Link>
+          </h3>
+          <p className="mt-1 text-sm text-left text-gray-500">{prod.category}</p>
+        </div>
+  
+        {prod.discount ? (
+          <div>
+            <p style={{ textDecoration: 'line-through' }} className="text-right font-medium text-gray-900">
+              {prod.old_price} TL
+            </p>
+            <div className="flex justify-between">
+              <p className="text-right font-medium text-gray-900">{prod.price} TL</p>
+            </div>
+          </div>
+        ) : (
+          <div className="space-y-6">
+            <p className="text-right font-medium text-gray-900">{prod.price} TL</p>
+          </div>
+        )}
+      </div>
+  
+      <div>
+        {cart.some((p) => p.product_id === prod.product_id) ? (
+          <div className="flex justify-end">
+            <button
+              onClick={() => {
+                dispatch({
+                  type: 'REMOVE_FROM_CART',
+                  payload: prod,
+                });
+              }}
+              className="px-4 py-2 font-bold text-red-500 bg-white rounded hover:bg-white"
+            >
+              Remove From Cart
+            </button>
+          </div>
+        ) : (
+          prod.stock <= 0 ? (
+            <button className="px-4 py-2 font-semibold text-gray-800 border border-gray-400 rounded cursor-not-allowed opacity-50 bg-white hover:bg-gray-100">
+              No Stock
+            </button>
+          ) : prod.stock === 1 ? (
+            <div className="flex items-center">
+              <p className="text-left italic text-sm text-gray-500">Only 1 left!</p>
+              <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="w-6 h-6 ml-2">
+                <path strokeLinecap="round" strokeLinejoin="round" d="M12 9v3.75m9-.75a9 9 0 11-18 0 9 9 0 0118 0zm-9 3.75h.008v.008H12v-.008z" />
+              </svg>
+              <div className="flex justify-end">
+                <button
+                  onClick={() => {
+                    dispatch({
+                      type: 'ADD_TO_CART',
+                      payload: prod,
+                    });
+                  }}
+                  className="text-right px-4 py-2 font-bold text-blue-700 bg-white rounded hover:bg-white"
+                >
+                  Add to Cart
+                </button>
+              </div>
+            </div>
+          ) : (
+            <div className="flex justify-end">
+
+                <button
+                  onClick={() => {
+                    dispatch({
+                      type: 'ADD_TO_CART',
+                      payload: prod,
+                    });
+                  }}
+                  className="text-right px-4 py-2 font-bold text-blue-700 bg-white rounded hover:bg-white"
+                >
+                  Add to Cart
+                </button>
+              </div>
+           
+          )
+        )}
+      </div>
+    </div>
+  );
+                };
