d464a5e7e5914b855d832e335cb6c72c20f23a07
Muge Yegin <mugeyegin@sabanciuniv.edu>
2023-06-11 20:47:12 +0300

    minor changes

--- client/react-app-with-tailwind/src/pages/createproduct.js
+++ client/react-app-with-tailwind/src/pages/createproduct.js
@@ -283,7 +283,7 @@ const Createp = () => {
       <div className="mt-6 flex items-center justify-center gap-x-6 sm:max-w-md">
         <button
           type="submit"
-          className="rounded-md bg-indigo-600 px-6 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+          className="rounded-md bg-dark-blue px-6 py-2 text-sm font-semibold text-white shadow-sm hover:bg-button-blue focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
         >
           Add
         </button>
